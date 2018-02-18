81a012fb6890954ee7a1158cd771ae94e8436317
brandii347 <brandi.nicole22@gmail.com>
2018-02-18 00:19:11 -0800

    everything

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+        name: "Han Solo",
+        photo: "https://nerdist.com/wp-content/uploads/2015/11/Han-Solo-Main.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Leia Organa",
+        photo: "https://am23.akamaized.net/tms/cnt/uploads/2017/08/leiatop1-650x574.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "https://i2-prod.mirror.co.uk/incoming/article9603913.ece/ALTERNATES/s810/PROD-Luke-Skywalker-in-Star-Wars-film.jpg",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Darth Vadar",
+        photo: "https://nerdist.com/wp-content/uploads/2016/11/Rogue-One-Vader.jpg",
+        scores: [
+            "3",
+            "2",
+            "2",
+            "5",
+            "1",
+            "4",
+            "2",
+            "3",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Boba Fett",
+        photo : "https://lumiere-a.akamaihd.net/v1/images/Boba-Fett_61fdadfd.jpeg?region=0%2C0%2C1200%2C675&width=768",
+        scores:[
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "3",
+            "5",
+            "2",
+            "4",
+            "1"
+        ]
+
+    },
+    {
+        name: "Obi-Wan Kenobi",
+        photo: "https://nerdist.com/wp-content/uploads/2017/12/download.jpg",
+        scores: [
+            "2",
+            "2",
+            "4",
+            "5",
+            "1",
+            "3",
+            "5",
+            "2",
+            "5",
+            "3"
+        ]
+
+    }
+];
+
+module.exports = friends;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,57 @@
+var friends = require("../data/friends.js");
+
+module.exports = function (app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res){
+
+        //object that has name and photo
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        console.log(req.body);
+
+        //result of the users survey POST and parse it
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        console.log(userScores);
+
+        var totalDifference = 0;
+
+//for loop to loop through all friends possibilities in database
+        for (var i=0; i < friends.length; i++){
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            //loop through all scores of each friend
+            for(var j = 0; j = friends[i].scores[j]; j++){
+
+                //calculate the difference b/t the scores and add them into totalDifference
+                //Math.abs returns absoulute value of given number
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                //if sum of difference is less then the differences of the current "best match"
+                if (totalDifference <= bestMatch.friendDifference){
+
+                    //resets the bestMatch to be the new friend
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+
+                }
+            }
+        }
+        //save users data to database (has to happen after the check or
+    // the database will return that the user is the user's best friend)
+    friends.push(userData);
+
+    //return JSON w/ the user's bestMatch. This will be used by the HTML in the next page
+    res.JSON(bestMatch);
+    })
+}
